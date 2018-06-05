c56c8135e517f133ec4ed8983460a235d30a9b6d
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-05 12:37:07 -0700

    Added Step and Ingredient to the Recipe class

--- app/src/main/java/bakingapp/udacity/com/bakingapp/Recipe.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/Recipe.java
@@ -1,21 +1,26 @@
 package bakingapp.udacity.com.bakingapp;
 
+import java.util.ArrayList;
+
 /**
  * A class to describe an object that holds recipe data in an easy-to-use way
  */
 public class Recipe {
     private Integer id;
     private String name;
+    private ArrayList<Ingredient> ingredients;
+    private ArrayList<Step> steps;
     private Integer servings;
     private String image;
 
     public Recipe() {
     }
 
-    public Recipe(Integer id, String name, Integer servings, String image) {
-
+    public Recipe(Integer id, String name, ArrayList<Ingredient> ingredients, ArrayList<Step> steps, Integer servings, String image) {
         this.id = id;
         this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
         this.servings = servings;
         this.image = image;
     }
@@ -34,6 +39,22 @@ public class Recipe {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<Step> steps) {
+        this.steps = steps;
     }
 
     public Integer getServings() {
