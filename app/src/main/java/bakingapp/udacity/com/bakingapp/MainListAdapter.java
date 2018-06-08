2d7de3a08cd48ce304f799ce029d356326b68e6f
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 09:23:26 -0700

    Fillded in getView method

--- app/src/main/java/bakingapp/udacity/com/bakingapp/MainListAdapter.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/MainListAdapter.java
@@ -1,9 +1,14 @@
 package bakingapp.udacity.com.bakingapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -13,10 +18,17 @@ import java.util.ArrayList;
 public class MainListAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Recipe> data;
+    private LayoutInflater layoutInflater;
 
+    /**
+     * The constructor where you initalized an instance of {@link MainListAdapter}
+     * @param c A valid context that can access a {@link LayoutInflater}
+     * @param d A set of recipes in the ArrayList form
+    */
     public MainListAdapter(Context c, ArrayList<Recipe> d) {
         context = c;
         data = d;
+        layoutInflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -36,6 +48,32 @@ public class MainListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView.getTag() == null) {
+            convertView = layoutInflater.inflate(R.layout.card_main, null);
+            MainListViewHolder holder = new MainListViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+        MainListViewHolder listHolder = (MainListViewHolder) convertView.getTag();
+
+        if (data.get(position).getImage().isEmpty()) {
+            listHolder.recipeImage.setImageResource(R.drawable.ic_baseline_chrome_reader_mode_24px);
+        } else {
+            Picasso.get().load(data.get(position).getImage()).into(listHolder.recipeImage);
+        }
+        listHolder.nameText.setText(data.get(position).getName());
+        listHolder.servingsText.setText(data.get(position).getServings());
+        return convertView;
+    }
+
+    public class MainListViewHolder {
+        ImageView recipeImage;
+        TextView nameText;
+        TextView servingsText;
+
+        public MainListViewHolder(View v) {
+            recipeImage = v.findViewById(R.id.iv_recipe_image);
+            nameText = v.findViewById(R.id.tv_name);
+            servingsText = v.findViewById(R.id.tv_servings);
+        }
     }
 }
