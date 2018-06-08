cc2ae4f04bef91ac957cc304efb0475eb6838387
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 11:33:29 -0700

    Added the Recipe activity template

---
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/RecipeActivity.java
@@ -0,0 +1,13 @@
+package bakingapp.udacity.com.bakingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
