e766a6a7e4c4cf8db4618e34c3f38a0e0af7e0a0
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 15:00:14 -0700

    Adding selectFragment template

---
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/SelectFragment.java
@@ -0,0 +1,27 @@
+package bakingapp.udacity.com.bakingapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A {@link Fragment} that displays information about a given recipe
+ */
+public class SelectFragment extends Fragment {
+
+
+    public SelectFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
