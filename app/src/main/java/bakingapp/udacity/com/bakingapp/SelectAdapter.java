2b9e2e363850d988ab58a5d79a5d2a00b9f4e27a
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 15:21:33 -0700

    Fillded in required methods

--- app/src/main/java/bakingapp/udacity/com/bakingapp/SelectAdapter.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/SelectAdapter.java
@@ -9,26 +9,42 @@ import android.widget.BaseAdapter;
  * A {@link BaseAdapter} that displays a recipe in a nice looking form
 */
 public class SelectAdapter extends BaseAdapter {
+
+    Recipe currentRecipe;
+    Context context;
+
     /**The constructor where an instance of {@link SelectAdapter} is made
      * @param c A context to access tools with
      * @param r A recipe to be displayed
     */
-    public SelectAdapter(Context c, Recipe r,) {
+    public SelectAdapter(Context c, Recipe r) {
+        currentRecipe = r;
+        context = c;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return currentRecipe.getSteps().size() + 1;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        if(position == 0){
+            return currentRecipe.getIngredients();
+        }
+        else{
+            return currentRecipe.getSteps().get(position - 1);
+        }
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        if(position == 0){
+            return 0;
+        }
+        else {
+            return currentRecipe.getSteps().get(position - 1).getId();
+        }
     }
 
     @Override
