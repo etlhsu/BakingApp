aeb65fde2849bc93ed58f949dd8b539173dbad06
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 20:13:57 -0700

    Filled in getView method

--- app/src/main/java/bakingapp/udacity/com/bakingapp/SelectAdapter.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/SelectAdapter.java
@@ -1,9 +1,11 @@
 package bakingapp.udacity.com.bakingapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 /**
  * A {@link BaseAdapter} that displays a recipe in a nice looking form
@@ -12,6 +14,7 @@ public class SelectAdapter extends BaseAdapter {
 
     Recipe currentRecipe;
     Context context;
+    LayoutInflater inflater;
 
     /**The constructor where an instance of {@link SelectAdapter} is made
      * @param c A context to access tools with
@@ -20,6 +23,7 @@ public class SelectAdapter extends BaseAdapter {
     public SelectAdapter(Context c, Recipe r) {
         currentRecipe = r;
         context = c;
+        inflater = LayoutInflater.from(c);
     }
 
     @Override
@@ -49,6 +53,60 @@ public class SelectAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.card_select,null);
+            convertView.setTag(new SelectViewHolder(convertView));
+        }
+        SelectViewHolder holder = (SelectViewHolder) convertView.getTag();
+        if(position == 0){
+            holder.headText.setText("Ingredients");
+            String displayText = "";
+            for(int i = 0; i < currentRecipe.getIngredients().size(); i++){
+                Ingredient currentIngredient = currentRecipe.getIngredients().get(i);
+                String unitText;
+                switch (currentIngredient.getUnit()){
+                    case CUPS:
+                        unitText = "cup(s) ";
+                        break;
+                    case TSP:
+                        unitText = "tablespoon(s) ";
+                        break;
+                    case TBLSP:
+                        unitText = "teaspoon(s) ";
+                        break;
+                    case K:
+                        unitText = "kilogram(s) ";
+                        break;
+                    case G:
+                        unitText = "gram(s) ";
+                        break;
+                    case OZ:
+                        unitText = "ounce(s) ";
+                    case UNIT:
+                        unitText = "";
+                }
+                String currentText = currentIngredient.getQuantity().toString() + " " +
+                        currentIngredient.getUnit() + currentIngredient.getName() + "\n";
+                displayText.concat(currentText);
+            }
+            holder.descriptText.setText(displayText);
+        }
+        else{
+            Step currentText = currentRecipe.getSteps().get(position - 1);
+            holder.headText.setText("Step " + String.valueOf(position - 1));
+            holder.descriptText.setText(currentText.getShortDescription());
+        }
+        return convertView;
+
+    }
+    public class SelectViewHolder {
+        private TextView headText;
+        private TextView descriptText;
+
+        public SelectViewHolder(View v) {
+            headText = v.findViewById(R.id.tv_header);
+            descriptText = v.findViewById(R.id.tv_description);
+        }
     }
 }
