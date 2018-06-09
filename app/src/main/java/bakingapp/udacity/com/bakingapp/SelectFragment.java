f8cc3d6f42fb4417dcb2549ad22477c4667b56c5
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-08 20:32:59 -0700

    Implemented adapter in the selectFragment

--- app/src/main/java/bakingapp/udacity/com/bakingapp/SelectFragment.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/SelectFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 
 
 /**
@@ -14,13 +16,27 @@ import android.view.ViewGroup;
  */
 public class SelectFragment extends Fragment {
 
+    Recipe currentRecipe;
 
     public SelectFragment() {
     }
 
-    @Nullable
+    @Override
+    public void setArguments(@Nullable Bundle args) {
+        currentRecipe = (Recipe) args.getSerializable("data");
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View returnView = inflater.inflate(R.layout.fragment_select,container);
+
+        RecyclerView list = returnView.findViewById(R.id.rv_info);
+
+        if(currentRecipe != null){
+            RecyclerView.Adapter adapter = new SelectAdapter(getContext(),currentRecipe);
+            list.setAdapter(adapter);
+        }
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
