5976616db7c96549a94bda8983b81f58eac91109
Ethan Hsu <ethan627hsu@gmail.com>
2018-06-11 14:04:01 -0700

    Bound the UI to the step data

--- app/src/main/java/bakingapp/udacity/com/bakingapp/RecipeFragment.java
+++ app/src/main/java/bakingapp/udacity/com/bakingapp/RecipeFragment.java
@@ -1,11 +1,35 @@
 package bakingapp.udacity.com.bakingapp;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 /**
@@ -15,18 +39,52 @@ public class RecipeFragment extends Fragment {
 
     Step currentStep;
 
+    public RecipeFragment() {
+    }
+
     @Override
     public void setArguments(@Nullable Bundle args) {
         currentStep = (Step) args.getSerializable("data");
-    }
-
-    public RecipeFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_recipe, container, false);
+        Context context = getContext();
+
+        View inflatedView = inflater.inflate(R.layout.fragment_recipe, container, false);
+
+        TextView descriptionTextView = inflatedView.findViewById(R.id.tv_recipe_description);
+        descriptionTextView.setText(currentStep.getDescription());
+
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        SimpleExoPlayer player =
+                ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+
+        PlayerView playerView = inflatedView.findViewById(R.id.ex_player);
+
+        playerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
+                Util.getUserAgent(context, "bakingapp"));
+
+        Uri uri = Uri.parse(currentStep.getVideoURL());
+        Log.v("URI",uri.toString());
+
+
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+
+        player.prepare(videoSource);
+
+        player.setRepeatMode(Player.REPEAT_MODE_ONE);
+
+        return inflatedView;
     }
 }
